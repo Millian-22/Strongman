811860b26c182265a51ce04f05a8e61f5260293a
Millian-22 <max.nelson22@gmail.com>
2024-07-05 08:26:51 -0400

    Working version with interval

--- src/app/nutrition/_nutrition/Chatbox.tsx
+++ src/app/nutrition/_nutrition/Chatbox.tsx
@@ -17,37 +17,28 @@ export const Chatbox = () => {
   const boxRef = useRef(null);
   const [inputValue, setInputValue] = useState<string>('');
   const [messages, setMessages] = useState<Messages[]>([]);
-  const [systemMessage, setSystemMessage] = useState<Messages[]>([]);
   const utils = api.useUtils();
   
 
 const {data: chatData} = api.openAI.getChat.useQuery(messages, {refetchOnWindowFocus: false});
-// const {data: chatData} = api.openAI.getChat.useQuery({});
 const sendMessage = api.openAI.sendChatMessage.useMutation({
   onMutate: async (newMessage) => {
-    //this might need to be set in state because it only returns one message
 
     await utils.openAI.getChat.cancel(messages);
+    const userMessage = newMessage[0]?.content;
 
-    const previousMessages = chatData?.join('');
-    console.log('previousMessages', previousMessages);
-    console.log('newMessage', newMessage);
-    const test = [newMessage];
-    utils.openAI.getChat.setData(messages, ((old) => {
-      console.log('old', old);
-      console.log('newMessage[0]?.content', newMessage[0]?.content);
-      if (boxRef.current) {
-        boxRef.current.innerHTML += ` ${newMessage[0].content}`;
-      }
+    if (!userMessage) { 
+      return;
+    }
 
-      return [newMessage[0]?.content ?? ''];
-      // const oldMsg = old?.join('');
-      // // return [{role: 'system', content: oldMsg}, newMessage[0]];
-      // return {
-      //   messages: newMessage,
-      // }
+    utils.openAI.getChat.setData(messages, (() => {
+      return [userMessage];
     }));
-    
+
+    if (boxRef.current) {
+      boxRef.current.innerHTML += `\n ${userMessage}`;
+    }
+
   },
   onSettled: async () => {
     await utils.openAI.getChat.invalidate(messages);
@@ -62,6 +53,12 @@ const sendMessage = api.openAI.sendChatMessage.useMutation({
 
   //this uses a ref but I might end up changing it so that 
   //I can just display the new message in the correct box? 
+  //can you set the interval on messages without a useEffect
+  //this shouldn't be a useEffect it isn't a side effect. That's theissue 
+  //but maybe if it were not awaiting the Data in the router it would work?
+  //okay so useQuery doesn't actually handle streaming all that well yet. It does 
+  //with server components if I convert the app, but I'm not sure it's worth it.
+  //maybe in a big refactor just to push myself in a little. For now I'm going to handle
   useEffect(() => {
     console.log('boxRef', boxRef);
     if (chatData) {
@@ -82,54 +79,52 @@ const sendMessage = api.openAI.sendChatMessage.useMutation({
         }
       }, 40); // Adjust the interval time to control the speed of word display
   
-      setSystemMessage([{role: 'system', content: chatData.join('')}]);
       return () => clearInterval(intervalId); // Cleanup interval on component unmount
     }
-  }, [chatData, messages]);
+  }, [chatData]);
 
   const onSubmitChat = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log('chatData', chatData);
     console.log('inputValue', inputValue);
     console.log('messages', messages);
-    console.log('systemMessage', systemMessage);
+    // const systemMessage = chatData?.map((message) => message.join(''))
+    //need a null handler here
+    const systemMsgTest = chatData ? chatData?.join(''): '';
     
     //should have this jsut take in the value and let the mutation change how this is sent to the query.
     sendMessage.mutate([{role: 'user', content: inputValue}]);
-    setMessages((prevMessage) => [...prevMessage, ...systemMessage,  {role: 'user', content: inputValue}] );
+    setMessages((prevMessages) => [...prevMessages, {role: 'system', content: systemMsgTest}, {role: 'user', content: inputValue}]);
     setInputValue('');
   }
-
-  //map through a list of the messages, it displays on left or right side depepding on the 'role'
-  //do I use a ref because I have the interval set up, do I use state, 
-  //How do I get just the new part to render, not re-render the entire list of messages already there.
-  //I think React is set up for this but it's time to check !  
-  //I might have this mutation pattern wrong in liftingLog as well. I think I need to add a mutation and keep that state
-  //I just remember it being slow though. 
-  //I think even in Hugo's best practices you eventually pass it off to state. So I get invalidation and keeping client / server state apart
-  //But I think you'd have to return to this eventually. And use state to handle the client interactions, even if you pass what they need
-  //as a mutation. 
-  //Whats the process. Initial useQuery returns the first stream. You type into box chatbox, it makes a mutation of the original call with a new message?
-  //Should only be a mutation the entire time, first mutation is called 
 
   return (
     <>
     <div className="flex flex-col w-full justify-center items-center h-1/2">
       <div className="flex flex-col w-2/3 h-full bg-gray-500 border border-slate-700 rounded-md">
-        <div className="pt-5 w-full px-4 flex-grow overflow-y-auto pb-5 whitespace-pre-wrap" ref={boxRef}></div>
+        <div className="pt-5 w-full px-4 flex-grow overflow-y-auto pb-5 whitespace-pre-wrap" ref={boxRef}>
+          {messages ? messages.map((messages, key) => { return (<div key={key}>{messages.content}</div>)}) : null}
+        </div>
         <form className="w-full px-4 pb-4" onSubmit={onSubmitChat}>
           <input placeholder="Chatbox" type="text" className="text-black border border-white-700 w-full" value={inputValue} onChange={(e) => handleInputChange(e.target.value)} />
         </form>
       </div>
     </div>
-    <div className="flex flex-col w-2/3 h-ful bg-white text-black">
+    {/* <div className="flex flex-col w-2/3 h-ful bg-white text-black">
       Test Box for Messages 
     {messages ? messages.map((message, key) => {
       return (<div key={key}>{message.content}</div>)
     }) : null
-    }
-    {/* <div>{chatData}</div> */}
     </div>
+    } */}
+    {/* this works. Only difference is getting the interval on here for only system messages */}
+    {/* And just like that son you've done it. Now just make the interval a function that works for chatData */}
+    {/* not a useEffect and you're goooooooodie */}
+    <div className="flex flex-col w-2/3 h-ful bg-white text-black overflow-y-auto">TEST 2
+    {messages ? messages.map((messages, key) => { return (<div key={key}>{messages.content}</div>)}) : null}
+      {chatData ? <div>{chatData.join('')}</div> : null }
+   </div>
+    
   </>
   )
 }
